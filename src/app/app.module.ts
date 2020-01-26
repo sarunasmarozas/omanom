920437f821da170f357dadb8d5d1353011cf2d7e
Sharis <sarezas@gmail.com>
2020-01-26 13:58:00 +0200

    master, init SSR, add Material, Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./app-level-components/header/header.component";
+import { MainComponent } from "./app-level-components/main/main.component";
+import { FooterComponent } from "./app-level-components/footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./feature-modules/material-module/material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, MainComponent, FooterComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
