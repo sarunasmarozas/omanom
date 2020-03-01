c1b6e707a58b473f521a9c3a846cd19abcfc300c
Sharis <sarezas@gmail.com>
2020-03-01 20:13:20 +0200

    reduce menu to two grid rows

--- src/app/app-level-components/main/main.component.ts
+++ src/app/app-level-components/main/main.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
+  cards = [
+    {
+      imageUrl: "../../../assets/images/pexels-1.jpg",
+      title: "",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magnam dolorem dicta eum itaque officiis inventore fugiat nesciunt, veniam vel?"
+    },
+    {
+      imageUrl: "../../../assets/images/pexels-2.jpg",
+      title: "",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magnam dolorem dicta eum itaque officiis inventore fugiat nesciunt, veniam vel?"
+    },
+    {
+      imageUrl: "../../../assets/images/pexels-3.jpg",
+      title: "",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magnam dolorem dicta eum itaque officiis inventore fugiat nesciunt, veniam vel?"
+    }
+  ];
 
-  constructor() { }
+  atLeastMd: boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.atLeastMd = window.innerWidth >= 768;
   }
 
+  ngOnInit() {
+    console.log(this.atLeastMd);
+  }
 }
