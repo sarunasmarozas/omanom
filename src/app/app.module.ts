b5c3ead1684c63bac562618e582abaf2a9924e83
sarezas <sarezas@gmail.com>
2020-04-26 14:24:15 +0300

    Remove header and gallery, add side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./app-level-components/header/header.component";
 import { MainComponent } from "./app-level-components/main/main.component";
 import { FooterComponent } from "./app-level-components/footer/footer.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./feature-modules/material-module/material.module";
-import { GalleryComponent } from "./app-level-components/main/components/gallery/gallery.component";
 import { GalleryService } from "./services/gallery.service";
+import { SideNavComponent } from './app-level-components/main/components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MainComponent,
     FooterComponent,
-    GalleryComponent
+    SideNavComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
