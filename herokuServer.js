5fbd01eb0b8c1ab0e8889bd666f450c5aad6ba0c
Sarunas M <sarunas.marozas@metasite.net>
2021-01-17 23:36:41 +0200

    fix deployment server file dist path

--- herokuServer.js
+++ herokuServer.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/monamo"));
+app.use(express.static(__dirname + "/dist/browser"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/monamo/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/browser/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
