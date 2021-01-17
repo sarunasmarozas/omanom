baa0cd207f2e2704cd216d9ce82dc9d62eecdb60
Sarunas M <sarunas.marozas@metasite.net>
2021-01-17 22:48:09 +0200

    fixes app responsive

--- src/app/app-level-components/main/components/about/about.component.ts
+++ src/app/app-level-components/main/components/about/about.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-about",
   templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.scss"]
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    // this.console.log();
-  }
+  ngOnInit() {}
 }
